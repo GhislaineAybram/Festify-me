f4e4fc72dd977b62584df44a7135fc8975e8e05d
Ghislaine <ghislaine.aybram@gmail.com>
2024-09-20 10:02:05 +0200

    essai 1

--- nuxt/tailwind.config.js
+++ nuxt/tailwind.config.js
@@ -1,25 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    '~/components/**/*.{vue,js,ts}',
-    '~/layouts/**/*.vue',
-    '~/pages/**/*.vue',
-    '~/composables/**/*.{js,ts}',
-    '~/plugins/**/*.{js,ts}',
-    '~/utils/**/*.{js,ts}',
-    '~/App.{js,ts,vue}',
-    '~/app.{js,ts,vue}',
-    '~/Error.{js,ts,vue}',
-    '~/error.{js,ts,vue}',
-    '~/app.config.{js,ts}'
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    // require('tailwindcss/nesting'),
-    // require('tailwindcss'),
-    // require('autoprefixer'),
   ],
 }
 
